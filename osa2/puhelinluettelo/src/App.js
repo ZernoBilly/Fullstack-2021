2db4162b52fa29fa9efe1486763cdc4fbca0a58e
ZernoBilly <heikki.kurppa@gmail.com>
2021-08-18 08:11:11 +0300

    removed dublicate kurssitiedot

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "";
 
 import Filter from "./components/Filter/Filter";
 import Input from "./components/Input/Input";
 import Numbers from "./components/Numbers/Numbers";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 123456678 },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([{}]);
   const [newName, setNewName] = useState({
     name: "",
     number: "",
@@ -42,6 +38,13 @@ const App = () => {
   const clear = () => {
     setNewName({ name: "", number: "" });
   };
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
